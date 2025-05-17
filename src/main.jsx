53136a81b9139e0eb3bd7d6069ea1630405706ec
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-17 01:28:51 -0400

    configured routes and added them to the DOM three

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './Components/Home.jsx'
+import Checkout from './Components/Checkout.jsx'
+import Search from './Components/Search.jsx'
+import { RouterProvider, createBrowserRouter , createRoutesFromElements} from 'react-router-dom'
+
+
+const router = createBrowserRouter([{
+  path : "/",
+  element : <App />,
+  children : [{index : true, element : <Home />},{path : "search", element : <Search />}, {path : "checkout", element : <Checkout />}]
+}])
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
