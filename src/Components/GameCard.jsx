a98708e3b56e39c917bc201d43f408f1ed9fcea9
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-19 19:36:26 -0400

    added  calculation for price, using metacritic, release year

--- src/Components/GameCard.jsx
+++ src/Components/GameCard.jsx
@@ -21,24 +21,41 @@ export default function GameCard({game}){
         p.textContent = temp;
         
     }
+    
+    let metacriticColor = game.rating >= 4 ? {backgroundColor : "#66CC33"} : game.rating >= 3 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
+    if (game.metacritic != null)
+        metacriticColor = game.metacritic >= 80 ? {backgroundColor : "#66CC33"} : game.metacritic >= 65 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
+    let price = 0;
+    let releasedYear = Number(game.released.substring(0,4));
+    let twoLastDigitsOfReleasedYear = Number(game.released.substring(2,4));
+    let currentYear = new Date().getFullYear();
+    let yearsPassedSinceStartOfCentury = currentYear - 2000;
+    let differenceBetween = currentYear - releasedYear;
+    if (differenceBetween <= yearsPassedSinceStartOfCentury){
+        price = twoLastDigitsOfReleasedYear / yearsPassedSinceStartOfCentury * 80 * (game.metacritic === null ? game.rating / 5 : game.metacritic / 100);
+        if (price < 10){
+            price = 10 * Number(game.metacritic) / 100;
+        }
+    }
+    else{
+        price = 10 * Number(game.metacritic) / 100;
+    }
 
-    let metacriticColor = game.metacritic >= 80 ? {backgroundColor : "#66CC33"} : game.metacritic >= 65 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
-
-
+    let imgContStyle = {background : "linear-gradient(to top, #000, transparent), center/cover no-repeat url(" + game.background_image + ")"}
     return <div className="GameCard">
-               <div className="imgCont">
-                <img src={game.background_image}/>
+               <div className="imgCont" style={imgContStyle}>
                 <div className="reactions" onClick={increaseAdd}>
                 <p className={"star" + game.id}>{game.added}</p>
                 <div className="star"></div>
                 </div>
                 <div className="metacritic" style={metacriticColor}>
-                    <p>{game.metacritic}</p>
+                    <p>{game.metacritic === null ? game.rating : game.metacritic}</p>
                 </div>
                </div>
                 <div className="GameDescription">
                         <h2 className="gameName">{game.name}</h2>
                         <p>Released: {game.released}</p>
+                        <p className="price">{price.toFixed(2)}$</p>
                         <div className="gameGenre">
                             {game.genres.map((genre)=> <div key={genre.id} className="genre">{genre.name}</div>)}
                         </div>
