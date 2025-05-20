7339233e9869c2b72ebebdc9124b872e380906d7
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-20 01:34:58 -0400

    added gamedetails as a route

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import App from './App.jsx'
 import Home from './Components/Home.jsx'
 import Checkout from './Components/Checkout.jsx'
 import Search from './Components/Search.jsx'
+import GameDetails , {gameDetailsLoader} from './Components/GameDetails.jsx'
 import { RouterProvider, createBrowserRouter , createRoutesFromElements} from 'react-router-dom'
 
 
 const router = createBrowserRouter([{
   path : "/",
   element : <App />,
-  children : [{index : true, element : <Home />},{path : "search", element : <Search />}, {path : "checkout", element : <Checkout />}]
+  children : [{index : true, element : <Home />},{path : "search", element : <Search />}, {path : "checkout", element : <Checkout />}, {path : ":id", element : <GameDetails />, loader : gameDetailsLoader}]
 }])
 
 
