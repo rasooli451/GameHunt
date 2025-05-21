af5139af732e971b150b520e4aaa92024dd39492
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-21 02:18:17 -0400

    changed route configurations, now everything works as intended, only thing left is checkout page

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import App from './App.jsx'
 import Home from './Components/Home.jsx'
 import Checkout from './Components/Checkout.jsx'
 import Search from './Components/Search.jsx'
+import SearchResult from './Components/SearchResult.jsx'
 import GameDetails , {gameDetailsLoader} from './Components/GameDetails.jsx'
+import Product from './Components/Product.jsx'
 import { RouterProvider, createBrowserRouter , createRoutesFromElements} from 'react-router-dom'
 
 
 const router = createBrowserRouter([{
   path : "/",
   element : <App />,
-  children : [{index : true, element : <Home />},{path : "search", element : <Search />}, {path : "checkout", element : <Checkout />}, {path : ":id", element : <GameDetails />, loader : gameDetailsLoader}]
+  children : [{index : true, element : <Home />},{path : "search", element : <Search />, children : [{path : ":searchterm", element : <SearchResult />}]}, {path : "checkout", element : <Checkout />}, {path: "product", element : <Product />, children : [{path : ":id", element : <GameDetails />, loader : gameDetailsLoader}]}]
 }])
 
 
