a0468e7b3896a8e320dfa53d2d2ef0f59eb2af9f
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-21 02:15:43 -0400

    implemented a detailed page for any game that is clicked on and styled it

--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -18,42 +18,92 @@ export default function GameDetails(){
 
     const [index, setIndex] = useState(0);
     const [count, setCount] = useState(0);
-    const game = useLoaderData();
-    let style = {background : "linear-gradient(to top, #000, transparent), center/cover no-repeat url(" + game.background_image + ")"};
-
+    const data = useLoaderData();
+    const game = data.game;
+    let screens = data.screenshots.results.map((object) => object.image);
+    let slideShow = [game.background_image, game.background_image_additional].concat(screens);
+    let style = {background : "linear-gradient(to top, #000, transparent), center/cover no-repeat url(" + slideShow[index] + ")"};
     function genreateString(array, isplatform){
-        let result = isplatform ? array[0].platform.name : array[0].name;
-        console.log(array);
-        for (let i = 1; i < array.length; i++){
-            result += ", " + (isplatform ? array[i].platform.name : array[i].name);
+        if (array.length > 0){
+            let result = isplatform ? array[0].platform.name : array[0].name;
+            for (let i = 1; i < array.length; i++){
+               result += ", " + (isplatform ? array[i].platform.name : array[i].name);
+            }
+           return result;
         }
-        return result;
+        return "unknown";
+    }
+
+    function calculatePrice(){
+        let price = 0;
+        let releasedYear = game.released == null ? 2010 : Number(game.released.substring(0,4));
+        let twoLastDigitsOfReleasedYear = game.released == null ? 10 : Number(game.released.substring(2,4));
+        let currentYear = new Date().getFullYear();
+        let yearsPassedSinceStartOfCentury = currentYear - 2000;
+        let differenceBetween = currentYear - releasedYear;
+        if (differenceBetween <= yearsPassedSinceStartOfCentury){
+            price = twoLastDigitsOfReleasedYear / yearsPassedSinceStartOfCentury * 80 * (game.metacritic === null ? game.rating / 5 : game.metacritic / 100);
+            if (price < 10){
+                price = 10 * Number(game.metacritic) / 100;
+            }
+        }
+        else{
+            price = 10 * Number(game.metacritic) / 100;
+        }
+        return price;
+    }
+    function handleIncrease(state, setState, className){
+        if (state == 0){
+            let element = document.querySelector("." + className);
+            element.removeAttribute("disabled");
+        }
+        if (className === "prevImg"){
+            if (state === slideShow.length - 1){
+                setState(0);
+            }
+            else{
+                setState(state + 1);
+            }
+        }
+        else{
+            setState(state + 1);
+        }
+    }
+
+    function handleDecrease(state, setState, className){
+        setState(function(prev){
+            if (prev == 1){
+                let element = document.querySelector("." + className);
+                element.disabled = "true"; 
+            }
+            setState(prev - 1);
+        })
     }
     let platforms = genreateString(game.platforms, true);
     let developers = genreateString(game.developers, false);
     let genres = genreateString(game.genres, false);
     let publishers = genreateString(game.publishers, false);
-    let price = 0;
-    let releasedYear = Number(game.released.substring(0,4));
-    let twoLastDigitsOfReleasedYear = Number(game.released.substring(2,4));
-    let currentYear = new Date().getFullYear();
-    let yearsPassedSinceStartOfCentury = currentYear - 2000;
-    let differenceBetween = currentYear - releasedYear;
-    if (differenceBetween <= yearsPassedSinceStartOfCentury){
-        price = twoLastDigitsOfReleasedYear / yearsPassedSinceStartOfCentury * 80 * (game.metacritic === null ? game.rating / 5 : game.metacritic / 100);
-        if (price < 10){
-            price = 10 * Number(game.metacritic) / 100;
-        }
-    }
-    else{
-        price = 10 * Number(game.metacritic) / 100;
-    }
+    let price = calculatePrice();
+    let metacriticColor = game.rating >= 4 ? {backgroundColor : "#66CC33"} : game.rating >= 3 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
+    if (game.metacritic != null)
+        metacriticColor = game.metacritic >= 80 ? {backgroundColor : "#66CC33"} : game.metacritic >= 65 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
     return <div className="gamePage">
         <div className="gameDetails">
             <h1>{game.name}</h1>
             <div className="details">
                 <div className="imgContainer" style={style}>
-            </div>
+                    <button className="nextImg" onClick={()=> handleIncrease(index, setIndex, "prevImg")}>&#10095;</button>
+                    <button className="prevImg" onClick={()=> handleDecrease(index, setIndex, "prevImg")} disabled={index == 0 ? true : false}>&#10094;</button>
+                    <div className="metacritic bigMetacritic" style={metacriticColor}>
+                    <p>{game.metacritic === null ? game.rating : game.metacritic}</p>
+                    </div>
+                    <div className="addSection">
+                        <button className="decrease" onClick={()=> handleDecrease(count, setCount, "decrease")} disabled={count == 0 ? true : false}>-</button>
+                        <div className="count"><p>{count}</p></div>
+                        <button className="increase" onClick={()=> handleIncrease(count, setCount, "decrease")}>+</button>
+                    </div>
+                    <button className="addToCart">Add To Cart</button>
+               </div>
             <div className="gameDescription">
                 <h2>Game Details</h2>
                 <p className="description">{game.description_raw}</p>
@@ -92,6 +142,8 @@ export default function GameDetails(){
 export async function gameDetailsLoader({params}){
     const {id} = params;
     const res = await fetch("https://api.rawg.io/api/games/" + id + "?key=d2cfad0807004f5c9a25a4ea2fcea8c6");
-
-    return res.json();
+    const screens = await fetch("https://api.rawg.io/api/games/" + id + "/screenshots?key=d2cfad0807004f5c9a25a4ea2fcea8c6");
+    let game = await res.json();
+    let screenshots = await screens.json();
+    return {game, screenshots};
 }
