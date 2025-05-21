92b504d0edf555ca0959683dd441d8930c06f85d
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-21 02:14:43 -0400

    refactored the code and made some adjustments

--- src/Components/GameCard.jsx
+++ src/Components/GameCard.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function GameCard({game}){
 
-    function increaseAdd(){
+    function increaseAdd(event){
+        event.stopPropagation();
         let p = document.querySelector(".star" + game.id);
         let star = document.querySelector(".star" + game.id + " + div");
         let temp = 0;
@@ -19,32 +20,35 @@ export default function GameCard({game}){
             star.style.backgroundColor = "gold";
         }
         p.textContent = temp;
-        
+    }
+
+    function calculatePrice(){
+        let price = 0;
+        let releasedYear = game.released == null ? 2010 : Number(game.released.substring(0,4));
+        let twoLastDigitsOfReleasedYear = game.released == null ? 10 : Number(game.released.substring(2,4));
+        let currentYear = new Date().getFullYear();
+        let yearsPassedSinceStartOfCentury = currentYear - 2000;
+        let differenceBetween = currentYear - releasedYear;
+        if (differenceBetween <= yearsPassedSinceStartOfCentury){
+            price = twoLastDigitsOfReleasedYear / yearsPassedSinceStartOfCentury * 80 * (game.metacritic === null ? game.rating / 5 : game.metacritic / 100);
+            if (price < 10){
+                price = 10 * Number(game.metacritic) / 100;
+            }
+        }
+        else{
+            price = 10 * Number(game.metacritic) / 100;
+        }
+        return price;
     }
     
     let metacriticColor = game.rating >= 4 ? {backgroundColor : "#66CC33"} : game.rating >= 3 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
     if (game.metacritic != null)
         metacriticColor = game.metacritic >= 80 ? {backgroundColor : "#66CC33"} : game.metacritic >= 65 ? {backgroundColor : "#FFCC33"} : {backgroundColor : "#FF0000"};
-    let price = 0;
-    let releasedYear = Number(game.released.substring(0,4));
-    let twoLastDigitsOfReleasedYear = Number(game.released.substring(2,4));
-    let currentYear = new Date().getFullYear();
-    let yearsPassedSinceStartOfCentury = currentYear - 2000;
-    let differenceBetween = currentYear - releasedYear;
-    if (differenceBetween <= yearsPassedSinceStartOfCentury){
-        price = twoLastDigitsOfReleasedYear / yearsPassedSinceStartOfCentury * 80 * (game.metacritic === null ? game.rating / 5 : game.metacritic / 100);
-        if (price < 10){
-            price = 10 * Number(game.metacritic) / 100;
-        }
-    }
-    else{
-        price = 10 * Number(game.metacritic) / 100;
-    }
-
+    let price = calculatePrice();
     let imgContStyle = {background : "linear-gradient(to top, #000, transparent), center/cover no-repeat url(" + game.background_image + ")"}
-    return <Link to={String(game.id)} className="gameLink"><div className="GameCard">
+    return <Link to={"/product/" + String(game.id)} className="gameLink"><div className="GameCard">
                <div className="imgCont" style={imgContStyle}>
-                <div className="reactions" onClick={increaseAdd}>
+                <div className="reactions" onClick={(e)=> increaseAdd(e)}>
                 <p className={"star" + game.id}>{game.added}</p>
                 <div className="star"></div>
                 </div>
