f693ca50e1346de12c6c61f4e7253c82a6883549
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-21 02:17:18 -0400

    fixed the genre links, now working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 
 
+import { useState } from "react";
 import checkout from "../assets/icons/checklist.png";
 import { Link } from "react-router-dom";
 
 export default function Navbar(){
 
-
+    const [typed, setTyped] = useState("");
     function dropdownAnimation(){
         let genres = document.querySelector(".genres");
         if (!genres.classList.contains("appear")){
@@ -17,30 +18,40 @@ export default function Navbar(){
             genres.classList.add("disappear");
         }
     }
+
+    function handleType(e){
+        setTyped(e.target.value);
+    }
     return <header className="navBarCont">
         <ul className="genres">
-                    <li>Action</li>
-                    <li>Adventure</li>
-                    <li>Horror</li>
-                    <li>Indie</li>
-                    <li>First-Person Shooter</li>
-                    <li>Third-Person Shooter</li>
-                    <li>Stealth</li>
-                    <li>fighting</li>
-                    <li >RPG</li>
-                    <li>Survival</li>
-                    <li>Sports</li>
-                    <li>Racing</li>
-                    <li>Puzzle</li>
+                    <Link to="/search/action"><li>Action</li></Link>
+                    <Link to="/search/adventure"><li>Adventure</li></Link>
+                    <Link to="/search/role-playing-games-rpg"><li >RPG</li></Link>
+                    <Link to="/search/strategy"><li >Strategy</li></Link>
+                    <Link to="/search/indie"><li>Indie</li></Link>
+                    <Link to="/search/shooter"><li>Shooter</li></Link>
+                    <Link to="/search/casual"><li>Casual</li></Link>
+                    <Link to="/search/simulation"><li>Simulation</li></Link>
+                    <Link to="/search/puzzle"><li>Puzzle</li></Link>
+                    <Link to="/search/arcade"><li>Arcade</li></Link>
+                    <Link to="/search/platformer"><li>Platformer</li></Link>
+                    <Link to="/search/massively-multiplayer"><li>Multiplayer</li></Link>
+                    <Link to="/search/racing"><li>Racing</li></Link>
+                    <Link to="/search/fighting"><li>Fighting</li></Link>
+                    <Link to="/search/family"><li>Family</li></Link>
+                    <Link to="/search/board-games"><li>Board Games</li></Link>
+                    <Link to="/search/card"><li>Card</li></Link>
+                    <Link to="/search/educational"><li>Educational</li></Link>
+                    <Link to="/search/sports"><li>Sports</li></Link>
             </ul>
         <div className="humberger" onClick={dropdownAnimation}>
             <span className="bar"></span>
         </div>
            <nav className="navbar">
-               <h3 className="name">GameHunt</h3>
+               <Link to={"/"}><h3 className="name">GameHunt</h3></Link>
                <div className="searchDiv">
-                <input type="text" placeholder="Search..." />
-                <button type="submit" className="searchBtn">Search</button>
+                <input type="text" placeholder="Search..." value={typed} onChange={handleType}/>
+                <Link to={"/search/typed=" + typed}><button className="searchBtn">Search</button></Link>
                </div>
                <Link to="checkout"><img className="checkoutLogo" src={checkout} alt="checkout"/></Link>
            </nav>
