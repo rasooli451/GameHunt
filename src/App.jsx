477209954b085598732b766407abc9c84f0dcbfe
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-23 03:20:11 -0400

    added a state for tracking carted games

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import './App.css'
 import Navbar from './Components/Navbar';
 
 export default function App() {
-  const apiKey = "d2cfad0807004f5c9a25a4ea2fcea8c6";
-  const link = "https://api.rawg.io/api/games/3328?key=";
 
+  const [cart, setCart] = useState([]);
+
+
+  
   return (<main>
         <Navbar />
         <Outlet />
