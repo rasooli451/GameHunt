a4c707ece97cfd543ea2f8584ada03c57a95f5be
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-24 02:23:57 -0400

    fixed an import statement

--- src/Components/SearchResult.jsx
+++ src/Components/SearchResult.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-import GameCard from "./gameCard";
+import GameCard from "./GameCard";
 import { useParams } from "react-router-dom";
 
 export default function SearchResult(){
