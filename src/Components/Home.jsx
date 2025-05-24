2d959cb9a08efe63c764cf08697b71a7dd98d240
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2025-05-24 02:04:08 -0400

    removed footer content and put it into its own component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,7 @@
 
 
 import { useEffect , useState} from "react";
-import { Link, NavLink } from "react-router-dom";
-import facebook from "../assets/icons/facebook.svg";
-import instagram from "../assets/icons/instagram.svg";
-import twitter from "../assets/icons/twitter.svg";
-import whatsapp from "../assets/icons/whatsapp.svg";
+import { Link} from "react-router-dom";
 import background from "../assets/backgrounds/HomepageBG.jpg";
 
 import GameCard from "./gameCard";
@@ -43,7 +39,7 @@ export default function Home(){
             <div className="welcome">
                 <h1>Hunt. Discover. Play.</h1>
                 <h3>Search thousands of games across all platforms and genres. Buy securely. Play instantly.</h3>
-                <h3>From AAA to indie gems — discover, compare, and buy your next game in seconds</h3>
+                <h3>From AAA to indie gems — discover, compare, and buy your next game in seconds.</h3>
             </div>
         </div>
         <div className="Top100">
@@ -76,51 +72,6 @@ export default function Home(){
             </div>
             </div>
         </div>
-        <footer>
-            <div className="footerContent">
-                <div className="slogan">
-                    <h1>GameHunt</h1>
-                    <p>GameHunt makes it easy to find and buy the perfect game — whether you're into strategy, shooters, or story-driven epics.</p>
-                    <div className="socialContacts">
-                        <img src={facebook} alt="facebook" className="socialLink"/>
-                        <img src={instagram} alt="instagram" className="socialLink"/>
-                        <img src={twitter} alt="twitter" className="socialLink"/>
-                        <img src={whatsapp} alt="whatsapp" className="socialLink"/>
-                    </div>
-                    <p>Built using <a href="https://rawg.io/apidocs">RAWG Gaming API</a></p>
-                    <p>Icons used from <a href="https://www.flaticon.com/free-icons/purchase">Flaticon</a></p>
-                </div>
-                <div className="garbage">
-                    <div className="list">
-                <p>Company</p>
-                <ul>
-                   <li>About</li>
-                   <li>Features</li>
-                   <li>Works</li>
-                   <li>Career</li> 
-                </ul>
-            </div>
-            <div className="list">
-                <p>Help</p>
-                <ul>
-                   <li>Customer Support</li>
-                   <li>Delievery Details</li>
-                   <li>Terms & Conditions</li>
-                   <li>Privacy Policy</li> 
-                </ul>
-            </div>
-            <div className="list">
-                <p>FAQ</p>
-                <ul>
-                   <li>Account</li>
-                   <li>Manage Deliveries</li>
-                   <li>Orders</li>
-                   <li>Payments</li> 
-                </ul>
-            </div>
-                </div>
-            </div>
-        </footer>
 
     </div>
 }
